17213418286a2d0713cd190444141999808fdf78
Luca Bobbio <bobbio.luca.117@gmail.com>
2024-01-14 08:11:37 -0300

    Add: Aura component

---
+++ pdf-visualforce/force-app/main/default/aura/invokeOrderPDF/invokeOrderPDFController.js
@@ -0,0 +1,6 @@
+({
+    newpagePDF: function(component, event, helper) {
+        //let newpage = window.open('/apex/GenerateOrderPDF?id=' + component.get("v.recordId"));
+        helper.showToast('success', 'Sucesso', 'PDF gerado com sucesso!')
+    }
+})
